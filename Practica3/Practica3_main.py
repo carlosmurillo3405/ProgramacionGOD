88c376bea84cc6ae3cea26a9518bbfb98061b340
WalterMs <carlos.murillo3405@alumnos.udg.mx>
2026-09-25 13:50:21 -0600

    Practica3 con logica de buble reparada xd

--- Practica3/Practica3_main.py
+++ Practica3/Practica3_main.py
@@ -3,9 +3,9 @@ import Practica3_data
 
 if __name__ == "__main__":
     contador_intentos = 0
-    while contador_intentos < 3:
-        usuario = str(input("Ingrese su nombre de usuario: ").lower())
-        if usuario in Practica3_data.USUARIO:
+    usuario = str(input("Ingrese su nombre de usuario: ").lower())
+    if usuario in Practica3_data.USUARIO:
+        while contador_intentos < 3:
             pin = int(input("Ingrese su PIN: "))
             if pin == Practica3_data.PINES[usuario]:
                 print("\033[32mAcceso concedido.\033[0m")
@@ -14,5 +14,7 @@ if __name__ == "__main__":
                 print(f"\033[31m PIN incorrecto. Intentos restantes: {2 - contador_intentos}\033[0m")
                 contador_intentos += 1
         else:
-            print(f"\033[31m No se reconoce el nombre de usuario\033[0m")
-    print("\033[31mSe han agotado los intentos. Acceso denegado.\033[0m")
+            print("\033[31mSe han agotado los intentos. Acceso denegado.\033[0m")
+    else:
+        print(f"\033[31m No se reconoce el nombre de usuario\033[0m")
+    
