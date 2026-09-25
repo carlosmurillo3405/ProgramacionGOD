328e322be7dc3cd1851acef8dd49b88ebfa31eef
WalterMs <carlos.murillo3405@alumnos.udg.mx>
2026-09-25 13:46:17 -0600

    Practica3 con feedback de acceso

--- Practica3/Practica3_main.py
+++ Practica3/Practica3_main.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
         if usuario in Practica3_data.USUARIO:
             pin = int(input("Ingrese su PIN: "))
             if pin == Practica3_data.PINES[usuario]:
+                print("\033[32mAcceso concedido.\033[0m")
                 Practica3_data.logica_retiros(usuario)
             else:
                 print(f"\033[31m PIN incorrecto. Intentos restantes: {2 - contador_intentos}\033[0m")
                 contador_intentos += 1
         else:
             print(f"\033[31m No se reconoce el nombre de usuario\033[0m")
+    print("\033[31mSe han agotado los intentos. Acceso denegado.\033[0m")
